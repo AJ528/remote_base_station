4d7f974b1ccde7f63cfc91b454881748241dc2e5
ratman528 <adevries528@gmail.com>
2024-09-15 19:54:15 -0500

    added script to allow black-magic debugging

--- debug2.sh
+++ debug2.sh
@@ -2,14 +2,14 @@
 
 # this bash script is called by "make debug"
 
-echo "START debug.sh"
-# call st-util to start the gdb server and listen on port 61234
-st-util -p 61234 &
+echo "START debug2.sh"
 
 # start the gdb client, connect to the remote server, load the program onto
 # the target, set a breakpoint at main, and enable the text user interface
 # also disable confirmations so you can quit by just using "q"
-gdb-multiarch -ex "set confirm off" -ex "target remote *:61234" \
-    -ex "load" -ex "break main" -ex "tui enable" bin/output.elf
+gdb-multiarch -ex "set confirm off" \
+    -ex "target extended-remote /dev/ttyACM0" -ex "monitor swdp_scan" \
+    -ex "attach 1" -ex "load" -ex "break main" \
+    -ex "tui enable" bin/output.elf
 
 echo "DONE"
